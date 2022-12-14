9077d2068b92b1d2e7300b2dbe35d6c60730fd70
AZIZBEKJURAEV <ajuraev98@mail.ru>
2022-12-14 08:50:18 +0500

    some new codes added

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import Header from "./Header";
 import Text from "./Text";
 import Footer from "./Footer";
 
-// function Page() {
-//   return (
-//     <div>
-//       <Header />
-//       <Text />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// ReactDOM.createRoot(<Page />, document.getElementById("root"));
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
